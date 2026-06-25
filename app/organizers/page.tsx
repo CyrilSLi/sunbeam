ae52d2de48df725b98ac0a15efd6324628ab7b0b
kat <katwang7749@gmail.com>
2026-06-25 15:02:43 -0400

    add frontend (not responsive)

--- app/organizers/page.tsx
+++ app/organizers/page.tsx
@@ -26,11 +26,90 @@ export default function Organizers() {
         <SidebarItem href="/organizers/docs" text="Docs" />
         <SidebarItem href="/organizers/docs" text="More Resources" />
         <SidebarItem href="/organizers/docs" text="Branding & Social Media" />
-        <SidebarItem href="/organizers/docs" text="Contact HQ" />   
-        <img src="/imgs/ray_back.png" alt="" className="w-5/8 -m-6"></img>                  
+        <SidebarItem href="/organizers/docs" text="Contact HQ" />
+        <button>
+          <img src="/imgs/ray_back.png" alt="" className="w-5/8 -m-6"></img>
+        </button>
       </div>
       {/* homepage */}
-      <div className="h-screen w-2/3"></div>
+      <div className="h-screen w-2/3 xl:p-12">
+        <h1 className="xl:text-6xl galindo text-transparent bg-clip-text bg-gradient-to-b from-orange-bright to-orange-dark">
+          Welcome, Sunbeamer!
+        </h1>
+        <div>
+          {/* 2 boxes row */}
+          <div className="flex justify-between">
+            {/* event countdown */}
+            <div className="glassbox-white w-1/2 xl:p-16 xl:m-8 xl:rounded-2xl text-center">
+              <h1 className="galindo xl:text-[150px] text-blue-bright">XX</h1>
+              <h3 className="galindo text-[32px] -mt-10 text-pink-dark">
+                days until the event!!!
+              </h3>
+            </div>
+            {/* check-in call countdown */}
+            <div className="glassbox-white w-1/2 xl:p-16 xl:m-8 xl:rounded-2xl text-center">
+              <h1 className="galindo xl:text-[150px] text-pink-dark">XX</h1>
+              <h3 className="galindo text-[32px] -mt-10 text-orange-dark">
+                days until the next check-in call
+              </h3>
+            </div>
+          </div>
+          {/* 2 boxes row */}
+          <div className="flex">
+            {/* weekly to-do */}
+            <div className="w-1/2 xl:mx-8">
+              <h3 className="galindo xl:text-4xl xl:m-8 text-blue-bright">
+                Week X To-Do
+              </h3>
+              <div className="h-[60vh] boardwalk xl:p-8 xl:mx-8 flex flex-col">
+                <p className="text-blue-dark outfit xl:text-3xl text-pretty xl:leading-12">
+                  <strong>1.</strong> Sign up to be an organizer in your city!
+                  (wait for approval - you’ll receive an email from us soon)
+                  <br /> <strong>2.</strong> once approved, join
+                  #sunbeam-organizers
+                  <br /> <strong>3.</strong> share feedback on our ULTIMATE
+                  ORGANIZER GUIDE
+                  <br /> <strong>4.</strong> join the very first check in call
+                  [date]
+                </p>
+                <a
+                  href="placeholder"
+                  className="text-blue-dark outfit xl:text-3xl text-pretty font-bold mt-auto mb-20"
+                >
+                  Click here to see the full 9-week plan
+                </a>
+              </div>
+            </div>
+            {/* check-in call link */}
+            <div className="w-1/2 xl:m-8">
+              <div className="glassbox-clear xl:p-8 h-1/2 flex justify-items-center rounded-t-2xl">
+                <img
+                  src="/imgs/ray1.png"
+                  alt=""
+                  className="mx-auto xl:p-4"
+                ></img>
+              </div>
+              <div className="glassbox-white flex flex-col p-12 rounded-b-2xl text-center">
+                <a
+                  href="placeholder"
+                  className="galindo xl:text-4xl text-orange-dark underline hover:decoration-wavy"
+                >
+                  Check-in Call Link
+                </a>
+                <h3 className="text-pink-dark outfit xl:text-2xl">
+                  meeting platform (zoom?)
+                </h3>
+                <h3 className="text-pink-dark outfit xl:text-2xl">
+                  00:00 EST - MM/DD
+                </h3>
+                <h3 className="text-blue-bright outfit xl:text-2xl">
+                  more details &#8680;
+                </h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
