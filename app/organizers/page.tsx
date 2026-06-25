6ec71c8566b038632b0fa41bd1ef2ad5680be6ab
kat <katwang7749@gmail.com>
2026-06-25 17:20:02 -0400

    it's still not responsive

--- app/organizers/page.tsx
+++ app/organizers/page.tsx
@@ -12,13 +12,16 @@ export default function Organizers() {
     >
       {/* sidebar */}
       <div
-        className="w-1/3 h-screen flex flex-col lg:px-4 xl:px-12 lg:py-8 xl:py-16 gap-8 xl:gap-14"
+        className="w-1/3 h-screen flex flex-col lg:px-4 2xl:px-12 lg:py-8 2xl:py-16 gap-8 2xl:gap-14"
         style={{
           backgroundImage: "url('/imgs/sidebar-water-desktop.png')",
           backgroundSize: "cover",
           backgroundPosition: "center",
         }}
       >
+        <div>
+
+        </div>
         <Link href="/">
           <img src="/imgs/logo_orgportal.png" className="w-3/4"></img>
         </Link>
@@ -32,23 +35,23 @@ export default function Organizers() {
         </button>
       </div>
       {/* homepage */}
-      <div className="h-screen w-2/3 xl:p-12">
-        <h1 className="xl:text-6xl galindo text-transparent bg-clip-text bg-gradient-to-b from-orange-bright to-orange-dark">
+      <div className="h-screen w-2/3 2xl:p-12">
+        <h1 className="2xl:text-6xl galindo text-transparent bg-clip-text bg-linear-to-b from-[#72BFDA] to-blue-bright">
           Welcome, Sunbeamer!
         </h1>
         <div>
           {/* 2 boxes row */}
           <div className="flex justify-between">
             {/* event countdown */}
-            <div className="glassbox-white w-1/2 xl:p-16 xl:m-8 xl:rounded-2xl text-center">
-              <h1 className="galindo xl:text-[150px] text-blue-bright">XX</h1>
+            <div className="glassbox-white w-1/2 2xl:p-16 2xl:m-8 2xl:rounded-2xl text-center">
+              <h1 className="galindo 2xl:text-[150px] text-blue-bright">XX</h1>
               <h3 className="galindo text-[32px] -mt-10 text-pink-dark">
                 days until the event!!!
               </h3>
             </div>
             {/* check-in call countdown */}
-            <div className="glassbox-white w-1/2 xl:p-16 xl:m-8 xl:rounded-2xl text-center">
-              <h1 className="galindo xl:text-[150px] text-pink-dark">XX</h1>
+            <div className="glassbox-white w-1/2 2xl:p-16 2xl:m-8 2xl:rounded-2xl text-center">
+              <h1 className="galindo 2xl:text-[150px] text-pink-dark">XX</h1>
               <h3 className="galindo text-[32px] -mt-10 text-orange-dark">
                 days until the next check-in call
               </h3>
@@ -57,12 +60,12 @@ export default function Organizers() {
           {/* 2 boxes row */}
           <div className="flex">
             {/* weekly to-do */}
-            <div className="w-1/2 xl:mx-8">
-              <h3 className="galindo xl:text-4xl xl:m-8 text-blue-bright">
+            <div className="w-1/2 2xl:mx-8">
+              <h3 className="galindo 2xl:text-4xl 2xl:m-8 text-blue-bright">
                 Week X To-Do
               </h3>
-              <div className="h-[60vh] boardwalk xl:p-8 xl:mx-8 flex flex-col">
-                <p className="text-blue-dark outfit xl:text-3xl text-pretty xl:leading-12">
+              <div className="h-[60vh] boardwalk 2xl:p-8 2xl:mx-8 flex flex-col">
+                <p className="text-blue-dark outfit 2xl:text-3xl text-pretty 2xl:leading-12">
                   <strong>1.</strong> Sign up to be an organizer in your city!
                   (wait for approval - you’ll receive an email from us soon)
                   <br /> <strong>2.</strong> once approved, join
@@ -74,35 +77,35 @@ export default function Organizers() {
                 </p>
                 <a
                   href="placeholder"
-                  className="text-blue-dark outfit xl:text-3xl text-pretty font-bold mt-auto mb-20"
+                  className="text-blue-dark outfit 2xl:text-3xl text-pretty font-bold mt-auto mb-20"
                 >
                   Click here to see the full 9-week plan
                 </a>
               </div>
             </div>
             {/* check-in call link */}
-            <div className="w-1/2 xl:m-8">
-              <div className="glassbox-clear xl:p-8 h-1/2 flex justify-items-center rounded-t-2xl">
+            <div className="w-1/2 2xl:m-8">
+              <div className="glassbox-clear 2xl:p-8 h-1/2 flex justify-items-center rounded-t-2xl">
                 <img
                   src="/imgs/ray1.png"
                   alt=""
-                  className="mx-auto xl:p-4"
+                  className="mx-auto 2xl:p-4"
                 ></img>
               </div>
               <div className="glassbox-white flex flex-col p-12 rounded-b-2xl text-center">
                 <a
                   href="placeholder"
-                  className="galindo xl:text-4xl text-orange-dark underline hover:decoration-wavy"
+                  className="galindo 2xl:text-4xl text-orange-dark underline hover:decoration-wavy"
                 >
                   Check-in Call Link
                 </a>
-                <h3 className="text-pink-dark outfit xl:text-2xl">
+                <h3 className="text-pink-dark outfit 2xl:text-2xl">
                   meeting platform (zoom?)
                 </h3>
-                <h3 className="text-pink-dark outfit xl:text-2xl">
+                <h3 className="text-pink-dark outfit 2xl:text-2xl">
                   00:00 EST - MM/DD
                 </h3>
-                <h3 className="text-blue-bright outfit xl:text-2xl">
+                <h3 className="text-blue-bright outfit 2xl:text-2xl">
                   more details &#8680;
                 </h3>
               </div>
@@ -119,10 +122,10 @@ export function SidebarItem({ href, text }: { href: string; text: string }) {
     <div>
       <Link href={href}>
         <div className="grid grid-cols-1 grid-rows-1 w-5/8">
-          <h2 className="col-start-1 row-start-1 outfit pink-outlined-text-drop-shadow lg:text-3xl xl:text-6xl font-bold">
+          <h2 className="col-start-1 row-start-1 outfit pink-outlined-text-drop-shadow lg:text-3xl 2xl:text-6xl font-bold">
             {text}
           </h2>
-          <h2 className="col-start-1 row-start-1 outfit pink-gradient-text lg:text-3xl xl:text-6xl font-bold z-10">
+          <h2 className="col-start-1 row-start-1 outfit pink-gradient-text lg:text-3xl 2xl:text-6xl font-bold z-10">
             {text}
           </h2>
         </div>
