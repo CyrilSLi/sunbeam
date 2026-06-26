61834e733bfb0752590fc1035ba077353afafda6
lolasanchezz <lolansanchez@icloud.com>
2026-06-26 15:59:11 -0400

    custom site works!

--- app/api/get-by-city/route.ts
+++ app/api/get-by-city/route.ts
@@ -13,7 +13,8 @@ export async function GET(request: Request) {
     filterByFormula: `{city} = "${city}"`,
   });
 
-  const url = `https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/${process.env.AIRTABLE_TABLE_ID}?${params}`;
+  const url = `https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/${process.env.AIRTABLE_EVENT_TABLE_ID}?${params}`;
+  //const url = `https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/${process.env.AIRTABLE_ORG_SIGNUP_TABLE_ID}?`;
 
   const res = await fetch(url, {
     headers: {
